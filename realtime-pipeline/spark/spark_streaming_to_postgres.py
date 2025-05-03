30c4168054dc460dbd8334bd40f81dafee034bac
DataAdvance <prince.agbenu@amalitechtraining.org>
2025-05-03 17:31:37 +0000

    updated dockerfile

--- realtime-pipeline/spark/spark_streaming_to_postgres.py
+++ realtime-pipeline/spark/spark_streaming_to_postgres.py
@@ -1,22 +1,30 @@
 from pyspark.sql import SparkSession
 from pyspark.sql.functions import to_timestamp, col
 
+
+# Load PostgreSQL connection details from a text file
 conn_details = {}
 with open("/opt/spark-apps/postgres_connection_details.txt") as f:
     for line in f:
         key, val = line.strip().split('=')
-        conn_details[key] = val
+        conn_details[key] = val          # Simple key-value parsing
 
+
+# Initialize a SparkSession with PostgreSQL JDBC driver
 spark = SparkSession.builder \
     .appName("StreamingToPostgres") \
     .config("spark.jars.packages", "org.postgresql:postgresql:42.2.18") \
     .getOrCreate()
 
+# Define the schema of the streaming CSV files
 schema = "event_id INT, user_id INT, product_id INT, event_type STRING, timestamp STRING"
-df = spark.readStream.option("header", True).schema(schema).csv("/opt/generated_data")
+
+# Read CSV files as a streaming source
+df = spark.readStream.option("header", True).schema(schema).csv("/opt/generated_data") # Watching this directory for new data
 
 df_transformed = df.withColumn("event_timestamp", to_timestamp(col("timestamp"))).drop("timestamp")
 
+# Function to write each micro-batch to PostgreSQL
 def write_to_postgres(batch_df, batch_id):
     batch_df.write \
         .format("jdbc") \
@@ -27,6 +35,9 @@ def write_to_postgres(batch_df, batch_id):
         .option("driver", "org.postgresql.Driver") \
         .mode("append") \
         .save()
-
+    
+# Set up the streaming query and begin writing to PostgreSQL for each batc
 query = df_transformed.writeStream.foreachBatch(write_to_postgres).start()
+
+# Keep the application running until manually stopped
 query.awaitTermination()
