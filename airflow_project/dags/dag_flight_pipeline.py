405f0a4d180fcf9e87dd4229b3140db3041a9ee3
DataAdvance <prince.agbenu@amalitechtraining.org>
2025-05-21 05:29:38 +0000

    included the export task dependency

--- airflow_project/dags/dag_flight_pipeline.py
+++ airflow_project/dags/dag_flight_pipeline.py
@@ -7,6 +7,7 @@ from datetime import datetime
 from tasks.load import load_csv_to_mysql
 from tasks.validate import validate_flight_data
 from tasks.transform import transform_and_compute_kpis
+from tasks.export import export_kpis_to_postgres
 
 # Define the DAG and task pipeline
 with DAG(
@@ -17,20 +18,30 @@ with DAG(
     tags=["flight", "mysql"]
 ) as dag:
 
+# The load task loads data from a CSV file into a MySQL database.
     task_load = PythonOperator(
         task_id='load_csv',
         python_callable=load_csv_to_mysql
     )
 
+# The validate task checks the data quality and integrity of the loaded data.
     task_validate = PythonOperator(
         task_id='validate_data',
         python_callable=validate_flight_data
     )
 
+# The transform task processes the data and computes key performance indicators (KPIs).
     task_transform = PythonOperator(
         task_id='transform_kpis',
         python_callable=transform_and_compute_kpis
     )
 
+# The export task exports the computed KPIs to a PostgreSQL database for analysis.S   
+    task_export = PythonOperator(
+    task_id='export_to_postgres',
+    python_callable=export_kpis_to_postgres
+)
+
     # Define task dependencies
-    task_load >> task_validate >> task_transform
+    task_load >> task_validate >> task_transform >> task_export
+
